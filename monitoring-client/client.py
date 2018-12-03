8b09aab541c8aad10666b33051b82ae5ff299d6d
Carlos Lucas <nosk14@gmail.com>
2018-12-04 00:18:31 +0100

    Added data buffer if request fails and minor fixes

--- monitoring-client/client.py
+++ monitoring-client/client.py
@@ -3,24 +3,12 @@ import logging as log
 
 
 class Client(object):
-    DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
     HEADERS = {'Content-Type': 'application/json'}
 
     def __init__(self, zone, endpoint):
         self.endpoint = endpoint
         self.zone = zone
-        self.dat_endpoint = "{}/data".format(self.endpoint)
 
-    def send_data(self, temperature, humidity, time):
-        data = {
-            "zone": self.zone,
-            "data": [
-                {
-                    "time": time.strftime(self.DATE_FORMAT),
-                    "temperature": temperature,
-                    "humidity": humidity
-                }
-            ]
-        }
+    def send_data(self, data):
         log.debug("Data sent: [{}]".format(str(data)))
-        requests.put(self.dat_endpoint, json=data, headers=self.HEADERS)
+        requests.put(self.endpoint, json=data, headers=self.HEADERS)
--- monitoring-client/monitor.py
+++ monitoring-client/monitor.py
@@ -6,36 +6,54 @@ from dht22 import DHT22
 from client import Client
 from datetime import datetime
 from pytz import timezone
+from requests.exceptions import RequestException
 
 GPIO.setmode(GPIO.BCM)
 
 
-class Monitor(object):
+class Monitor:
     TZ = timezone('Europe/Madrid')
     DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
     def __init__(self, pin, zone, api_endpoint, freq):
-        super().__init__()
         self.__dht = DHT22(pin)
         self.frequency = freq
         self.zone = zone
         self.client = Client(zone, api_endpoint)
 
     def run(self):
+        pending_data = []
         while True:
             h, t, e = self.__dht.read_data()
             if not e:
-                self.client.send_data(t, h, datetime.now(self.TZ))
-                log.info("Data sent correctly [{}, {}, {}]".format(self.zone, t, h))
+                pending_data.append(self.build_data(t, h, datetime.now(self.TZ)))
+                try:
+                    self.client.send_data(pending_data)
+                    log.info("Data sent correctly [{}, {}, {}]".format(self.zone, t, h))
+                    pending_data = []
+                except RequestException as ex:
+                    log.error("Error sending data: [{}]".format(ex))
                 sleep(self.frequency)
             else:
                 log.warning("Error reading data [{}, {}, {}]".format(self.zone, t, h))
                 sleep(30)
 
+    def build_data(self, temperature, humidity, time):
+        return {
+                    "time": time.strftime(self.DATE_FORMAT),
+                    "temperature": temperature,
+                    "humidity": humidity
+                }
+
+    def build_request(self, data):
+        return {
+            "zone": self.zone,
+            "data": data
+        }
 
 if __name__ == '__main__':
     frequency = os.environ.get("FREQUENCY", 600)
-    endpoint = os.environ.get("ENDPOINT", 'http://localhost:5000')
+    endpoint = os.environ.get("ENDPOINT", 'http://localhost:5000/data')
     pin = os.environ.get("PIN", 4)
     zone = os.environ.get("ZONE")
     if not zone:
